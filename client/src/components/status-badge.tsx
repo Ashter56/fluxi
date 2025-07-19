67c84596ab30ffb6428f533f472a6e47e99e53f8
Ashter56 <ashtera405@gmail.com>
2025-07-19 18:44:44 +0330

    Update status-badge.tsx

--- client/src/components/status-badge.tsx
+++ client/src/components/status-badge.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { type TaskStatus } from "@shared/schema";
+import { type TaskStatus } from "../shared/schema";
 
 interface StatusBadgeProps {
   status: TaskStatus;
