f762464a174ae06888af07941d681c292c21a39b
Ashter56 <ashtera405@gmail.com>
2025-09-17 07:57:53 +0330

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -97,8 +97,8 @@ export const tasksRelations = relations(tasks, ({ one, many }) => ({
   likes: many(likes),
 }));
 
-export const commentsRelations = relations(comments, ({ one }) => ({
-  user: one(users, { fields: [comments.userId], references: [users.id] }),
+export极 const commentsRelations = relations(comments, ({ one }) => ({
+  user: one(users, { fields: [comments.userId], references极: [users.id] }),
   task: one(tasks, { fields: [comments.taskId], references: [tasks.id] }),
 }));
 
